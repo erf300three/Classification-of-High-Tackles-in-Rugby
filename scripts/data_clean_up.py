507419d368b1e6c24cb021b738a31ce3b408ce9f
Ethan Farrell <Ethan.Farrell@warwick.ac.uk>
2023-12-23 20:57:53 +0000

    RUCK-2: Explore how to modify mp4 files

---
+++ scripts/data_clean_up.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+
+print("Hello World!")
+
+# Create random np array
+np.random.seed(0)
+np_array = np.random.randint(0, 10, size=(5, 5))
+print(np_array)
