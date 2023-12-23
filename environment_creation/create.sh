be3cce30aa3c212d8cb6b5ee10b68454daf00ec4
Ethan Farrell <Ethan.Farrell@warwick.ac.uk>
2023-12-23 20:56:48 +0000

    RUCK-1: Add environment creation script and requirements file

---
+++ environment_creation/create.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Load the required python version
+module load Python/3.11.0
+
+# Create a virtual environment
+if [[ $# == 1 ]]; then 
+    # Remove the virtual environment if first argument is "clean"
+    if [[ $1 == "clean" ]]; then
+        echo "Removing virtual environment"
+        rm -rf Classification-of-High-Tackles-in-Rugby/venv
+        echo "Virtual environment removed"
+    # Update the virtual environment if first argument is "update"
+    elif [[ $1 == "update" ]]; then 
+        # Activate the virtual environment
+        source Classification-of-High-Tackles-in-Rugby/venv/bin/activate
+        # Upgrade pip
+        pip install --upgrade pip
+        # Install the required packages
+        pip install -r Classification-of-High-Tackles-in-Rugby/environment_creation/requirements.txt
+        echo "Required packages installed"
+        # Deactivate the virtual environment
+        deactivate
+    else
+        echo "Invalid argument"
+        echo "Usage: ./create.sh [clean]"
+        exit 1
+    fi
+# If no arguments are provided, create the virtual environment if it does not exist
+else 
+    if ! [ -d "Classification-of-High-Tackles-in-Rugby/venv" ]; then
+        python3 -m venv --system-site-packages Classification-of-High-Tackles-in-Rugby/venv
+        echo "Virtual environment created"
+        # Activate the virtual environment
+        source Classification-of-High-Tackles-in-Rugby/venv/bin/activate
+        echo "Virtual environment activated"
+
+        # Display the python version
+        python3 --version
+
+        # Upgrade pip 
+        pip install --upgrade pip 
+
+        # Install the required packages
+        pip install -r Classification-of-High-Tackles-in-Rugby/environment_creation/requirements.txt
+        echo "Required packages installed"
+        deactivate
+    else
+        echo "Virtual environment already exists"
+    fi
+fi
+
+
+
+
+
