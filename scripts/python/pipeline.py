f8c831bf73ba8eeb5111af97d54334353c684ea4
Ethan Farrell <Ethan.Farrell@warwick.ac.uk>
2024-03-04 15:50:00 +0000

    RUCK-19: Add pose classification model into the pipeline
    * Add the final stage of the pipeline to classify the poses of the
      players in the tackle region
    * Add a function to compute the distances between the players
      keypoints
    * Refactor old parts of pipeline to make the code more reliable and
      easier to understand

--- scripts/python/pipeline.py
+++ scripts/python/pipeline.py
@@ -1,20 +1,24 @@
 import os
 import sys
 import cv2
+import moviepy.editor as moviepy
 import csv
 import pandas as pd
 import numpy as np
 import torch
 from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
 
 from action_recogniser_model import ActionRecogniserModel, action_prediction
+from pose_classifier_model import PoseClassifierModel, tackle_classification
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Computes the normalised distance between player1's estimated head location and player2's keypoints
 def compute_player_distances(player1, player2):
     player2_distances = []
-    player1_head_x, player1_head_y = 0
+    player1_head_x = 0
+    player1_head_y = 0
     player1_divisor = 0
     # The first 5 key points are: nose, left eye, right eye, left ear and right ear which we will use to estimate the head location
     for i in range(5):
@@ -76,7 +80,7 @@ def pipeline(video_path):
     action_model = torch.load("/dcs/large/u2102661/CS310/models/activity_recogniser/r3d_18/run4/best.pt", map_location=device).to(device)
 
     # Determine if any of the frames are likely to contain a tackle using our action recogniser model
-    frames = action_prediction(action_model, video_path)
+    frames = action_prediction(action_model, video_path, is_full_version=False)
 
     
     if len(frames) == 0:
@@ -105,7 +109,8 @@ def pipeline(video_path):
         fps = cap.get(cv2.CAP_PROP_FPS)
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out = cv2.VideoWriter(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_clips/{video_path.split('/')[-1][:-4]}_{clip[0]}_{clip[-1]}.mp4", fourcc, fps, (width, height))
+        clip_name = f"{video_path.split('/')[-1][:-4]}_{clip[0]}_{clip[-1]}.mp4"
+        out = cv2.VideoWriter(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_clips/{clip_name}", fourcc, fps, (width, height))
 
         for frame in clip:
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
@@ -113,7 +118,11 @@ def pipeline(video_path):
             out.write(img)
         out.release()
 
-
+        # Conver the video to h264 format using moviepy
+        clip = moviepy.VideoFileClip(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_clips/{clip_name}")
+        clip.write_videofile(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_clips/{clip_name[:-4]}_h264.mp4", codec="libx264", audio_codec="aac")
+        # Delete the original video
+        os.remove(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_clips/{clip_name}")
     
     # If we have detected a tackle we will use a custom YOLOv8 model to detect the bounding box of the tackle 
     yolo = YOLO('/dcs/large/u2102661/CS310/models/tackle_location/train/weights/best.pt')
@@ -126,16 +135,16 @@ def pipeline(video_path):
             # Load the frame
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
             ret, img = cap.read()
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             results = yolo.predict(img, save=False, show=False, project="/dcs/large/u2102661/CS310/model_evaluation/pipeline", name="tackle_location")
             # Save the original frame
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
             if not os.path.exists(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_frames/{video_path.split('/')[-1][:-4]}"):
                 os.makedirs(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_frames/{video_path.split('/')[-1][:-4]}")
             cv2.imwrite(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_frames/{video_path.split('/')[-1][:-4]}/frame_{frame}.jpg", img)
             # Plot the results on the image
             annotated_frame = results[0].plot()
-            annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR)
+            # annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR)
             if not os.path.exists(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_location/{video_path.split('/')[-1][:-4]}"):
                 os.makedirs(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_location/{video_path.split('/')[-1][:-4]}")
             cv2.imwrite(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_location/{video_path.split('/')[-1][:-4]}/frame_{frame}.jpg", annotated_frame)
@@ -168,7 +177,7 @@ def pipeline(video_path):
         # Get the image that we want to crop
         cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
         ret, img = cap.read()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # Prevent the padding from going outside of the image
         new_x1 = max(0, int(x1 - padding_x))
         new_x2 = min(img.shape[1], int(x2 + padding_x))
@@ -176,7 +185,7 @@ def pipeline(video_path):
         new_y2 = min(img.shape[0], int(y2 + padding_y))
         # Crop the image
         cropped_img = img[new_y1:new_y2, new_x1:new_x2]
-        cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR)
+        # cropped_img = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR)
         # Save the cropped image
         if not os.path.exists(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_crops/{video_path.split('/')[-1][:-4]}"):
             os.makedirs(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_crops/{video_path.split('/')[-1][:-4]}")
@@ -196,7 +205,7 @@ def pipeline(video_path):
             imgsz=640
         )
         annotated_frame = results[0].plot()
-        annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR)
+        # annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR)
         if not os.path.exists(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/pose_estimation/{video_path.split('/')[-1][:-4]}"):
             os.makedirs(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/pose_estimation/{video_path.split('/')[-1][:-4]}")
         cv2.imwrite(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/pose_estimation/{video_path.split('/')[-1][:-4]}/{frame}", annotated_frame)
@@ -212,19 +221,19 @@ def pipeline(video_path):
                 temp3 = tackle_localisation[frame_number][0].to("cpu").numpy()[0]
                 x1, y1, x2, y2 = round(temp3[0]), round(temp3[1]), round(temp3[2]), round(temp3[3])
                 
-                # # Check if there are any keypoints
-                # print("length of keypoints: ", len(r.keypoints))
-                # print("keypoints: ", r.keypoints)
-                # print("length of xy: ", len(r.keypoints[0].xy.to("cpu").numpy()))
-                # print("xy: ", r.keypoints[0].xy.to("cpu").numpy())
                 # # Compare to the shape of empty keypoints
-                # print("condition:", r.keypoints[0].xy.to("cpu").shape == (1, 0, 2))
                 if r.keypoints[0].xy.to("cpu").shape == (1, 0, 2):
                     continue
 
-                for player_keypoint in r.keypoints:
+                players_in_tackle = []
+                players_in_tackle_normalised = []
+                players_in_tackle_confidence = []
 
+                pose_classification_model = torch.load("/dcs/large/u2102661/CS310/models/pose_estimation/run1/last.pt", map_location=device).to(device)
+                for player_keypoint in r.keypoints:
                     points = player_keypoint.xy.to("cpu").numpy()[0]
+                    points_normalised = player_keypoint.xyn.to("cpu").numpy()[0]
+                    confidence = player_keypoint.conf.to("cpu").numpy()
                     print("points: ", points)
                     # Check if the hip or shoulder keypoints are within the bounding 
                     # The hip shoulder points are index 11 and 12 and the shoulder points are index 5 and 6
@@ -243,7 +252,59 @@ def pipeline(video_path):
                             print(f"Point: {point} is not within the bounding box")
                     if or_gate:
                         print("Player is within the bounding box")
-                        print(points)
+                        players_in_tackle.append(points)
+                        players_in_tackle_normalised.append(points_normalised)
+                        players_in_tackle_confidence.append(confidence)
+                        
+
+                    if len(players_in_tackle) < 2:
+                        print("Not enough players in the tackle")
+                        continue
+                        
+                    for play1 in range(len(players_in_tackle)):
+                        for play2 in range(play1 + 1, len(players_in_tackle)):
+                            player1 = players_in_tackle[play1]
+                            player2 = players_in_tackle[play2]
+                            player1_normalised = players_in_tackle_normalised[play1]
+                            player2_normalised = players_in_tackle_normalised[play2]
+                            player1_confidence = players_in_tackle_confidence[play1]
+                            player2_confidence = players_in_tackle_confidence[play2]
+
+                            img = cv2.imread(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/tackle_frames/{video_path.split('/')[-1][:-4]}/{frame}")
+
+                            player1 = np.insert(player1, 2, player1_confidence, axis=1)
+                            player2 = np.insert(player2, 2, player2_confidence, axis=1)
+                            annotator = Annotator(img, line_width=2, font_size=1.0)
+
+                            # Draw the annotations
+                            annotator.kpts(player1)
+                            annotator.kpts(player2)
+                            out_img = annotator.result()
+
+                            player1_distances = compute_player_distances(player1_normalised, player2_normalised)
+                            player2_distances = compute_player_distances(player2_normalised, player1_normalised)
+
+
+                            # We want to classify the pose of the players in the tackle
+                            predicted_class = tackle_classification(pose_classification_model, player1_distances, player2_distances, device)
+                            # We want to write the classification onto the image
+                            # This is in BGR format
+                            if predicted_class == 0:
+                                cv2.putText(out_img, "No Tackle", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                            elif predicted_class == 1:
+                                cv2.putText(out_img, "Low Tackle", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                            elif predicted_class == 2:
+                                cv2.putText(out_img, "High Tackle", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+                            # Save the image
+                            if not os.path.exists(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/pose_classification/{video_path.split('/')[-1][:-4]}"):
+                                os.makedirs(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/pose_classification/{video_path.split('/')[-1][:-4]}")#
+                            cv2.imwrite(f"/dcs/large/u2102661/CS310/model_evaluation/pipeline/pose_classification/{video_path.split('/')[-1][:-4]}/{frame[:-4]}_player{play1}_player{play2}.jpg", out_img)
+
+
+
+
+
             
 
                     
@@ -258,6 +319,7 @@ def pipeline(video_path):
 
 def main():
     dir_path = "/dcs/large/u2102661/CS310/datasets/negative_clips"
+    # dir_path = "/dcs/large/u2102661/CS310/datasets/anomaly_detection/original_videos"
     for file in os.listdir(dir_path):
         if file.endswith(".mp4"):
             video_path = os.path.join(dir_path, file)
