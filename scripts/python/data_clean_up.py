39e73e28f302c54e76a6a68ba2028c8ac83af8ae
Ethan Farrell <Ethan.Farrell@warwick.ac.uk>
2024-04-27 17:14:14 +0100

    Refactor code using a linter

--- scripts/python/data_clean_up.py
+++ scripts/python/data_clean_up.py
@@ -1,37 +1,38 @@
-import numpy as np
-import pandas as pd
 import os
-import sys
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-from moviepy.editor import * 
-import ffmpeg
 import argparse
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import *
 
-def get_all_vids(dir):
+# These functions were created at the start of the project when we were experimenting with different
+# data manipulation techniques. We decided to use the moviepy library for video manipulation as it was
+# more flexible and easier to use than the opencv library. We used the moviepy library to trim, resize,
+# flip and crop videos. We also used the os library to rename video files to a uniform naming convention.
+
+def get_all_vids(in_dir):
     """
     Returns a list of all video files in the directory
     :param dir: directory to search
     :return: list of video files
     """
     vids = []
-    for file in os.listdir(dir):
+    for file in os.listdir(in_dir):
         if file.endswith(".mp4"):
             # Replace with uniform naming convention for all video files.
             file = file.replace("-", "")
             file = file.replace("  ", "_")
             file = file.replace(" ", "_")
-            file = os.path.join(dir, file)
+            file = os.path.join(in_dir, file)
             vids.append(file)
     return vids
 
-def rename_all_vids(dir):
+def rename_all_vids(in_dir):
     """
     Renames all video files in the directory to a uniform naming convention
     :param dir: directory to search
     :return: list of video files
     """
-    base_path = os.path.abspath(dir)
-    for file in os.listdir(dir):
+    base_path = os.path.abspath(in_dir)
+    for file in os.listdir(in_dir):
         if file.endswith(".mp4"):
             # Replace with uniform naming convention for all video files.
             temp = file.replace("-", "")
@@ -40,7 +41,7 @@ def rename_all_vids(dir):
             os.rename(base_path + "/" + file, base_path + "/" + temp)
 
 
-def trim_all_vids(dir, out_dir, start_time, end_time):
+def trim_all_vids(in_dir, out_dir, start_time, end_time):
     """
     Trims all videos in the directory
     :param dir: directory to search
@@ -48,18 +49,18 @@ def trim_all_vids(dir, out_dir, start_time, end_time):
     :param end_time: end time of the trim
     :return: whether the operation was successful
     """
-    base_path = os.path.abspath(dir) 
+    base_path = os.path.abspath(in_dir)
     output_path = base_path[:base_path.find("data") + 4] + out_dir
     base_path = base_path + "/"
     print(base_path)
     print(output_path)
-    for file in os.listdir(dir):
+    for file in os.listdir(in_dir):
         if file.endswith(".mp4"):
             print(file)
-            ffmpeg_extract_subclip(base_path + file, start_time, end_time, targetname=(output_path + file[:-4] + "_trimmed.mp4"))
+            ffmpeg_extract_subclip(base_path + file, start_time, end_time, targetname=f"{output_path}{file[:-4]}_trimmed.mp4")
     return True
 
-def change_resolution(dir, out_dir, x_res, y_res):
+def change_resolution(in_dir, out_dir, x_res, y_res):
     """
     Changes the resolution of all videos in the directory
     :param dir: directory to search
@@ -67,12 +68,12 @@ def change_resolution(dir, out_dir, x_res, y_res):
     :param y_res: y resolution
     :return: whether the operation was successful
     """
-    base_path = os.path.abspath(dir)
+    base_path = os.path.abspath(in_dir)
     output_path = base_path[:base_path.find("data") + 4] + out_dir
     base_path = base_path + "/"
     print(base_path)
     print(output_path)
-    for file in os.listdir(dir):
+    for file in os.listdir(in_dir):
         if file.endswith(".mp4"):
             print(file)
             clip = VideoFileClip(base_path + file)
@@ -80,18 +81,18 @@ def change_resolution(dir, out_dir, x_res, y_res):
             clip_resized.write_videofile(output_path + file[:-4] + "_resized.mp4")
     return True
 
-def flip_horizontally(dir, out_dir):
+def flip_horizontally(in_dir, out_dir):
     """
     Flips all videos in the directory horizontally
     :param dir: directory to search
     :return: whether the operation was successful
     """
-    base_path = os.path.abspath(dir)
+    base_path = os.path.abspath(in_dir)
     output_path = base_path[:base_path.find("data") + 4] + out_dir
     base_path = base_path + "/"
     print(base_path)
     print(output_path)
-    for file in os.listdir(dir):
+    for file in os.listdir(in_dir):
         if file.endswith(".mp4"):
             print(file)
             clip = VideoFileClip(base_path + file)
@@ -99,7 +100,7 @@ def flip_horizontally(dir, out_dir):
             clip_flipped.write_videofile(output_path + file[:-4] + "_flipped.mp4")
     return True
 
-def crop_specific_region(dir, out_dir, width, height, x, y):
+def crop_specific_region(in_dir, out_dir, width, height, x, y):
     """
     Crops all videos in the directory to a specific region
     :param dir: directory to search
@@ -109,12 +110,12 @@ def crop_specific_region(dir, out_dir, width, height, x, y):
     :param y: y coordinate of the cropped region
     :return: whether the operation was successful
     """
-    base_path = os.path.abspath(dir)
+    base_path = os.path.abspath(in_dir)
     output_path = base_path[:base_path.find("data") + 4] + out_dir
     base_path = base_path + "/"
     print(base_path)
     print(output_path)
-    for file in os.listdir(dir):
+    for file in os.listdir(in_dir):
         if file.endswith(".mp4"):
             print(file)
             clip = VideoFileClip(base_path + file)
@@ -124,8 +125,20 @@ def crop_specific_region(dir, out_dir, width, height, x, y):
 
 def main():
     """
-    Main function
-    :return: true if successful data manipulation is completed
+    This is the main function that parses the arguments and calls the appropriate function
+
+    Arguments:
+        --op: string, operation to perform on the data (rename, trim, resize, flip, crop)
+        --dir: string, directory to perform the operation on
+        --out_dir: string, output directory
+        --start_time: float, start time of the trim (in seconds)
+        --end_time: float, end time of the trim (in seconds)
+        --x_res: int, x resolution (in pixels)
+        --y_res: int, y resolution (in pixels)
+        --width: int, width of the cropped region (in pixels)
+        --height: int, height of the cropped region (in pixels)
+        --x: int, x coordinate of the cropped region (in pixels)
+        --y: int, y coordinate of the cropped region (in pixels)
     """
 
     parser = argparse.ArgumentParser(description='Manipulate data')
@@ -141,7 +154,7 @@ def main():
     parser.add_argument("--x", type=int, help="X coordinate of the cropped region") # in pixels (crop)
     parser.add_argument("--y", type=int, help="Y coordinate of the cropped region") # in pixels (crop)
     args = parser.parse_args()
-    
+
     match args.op:
         case "rename":
             rename_all_vids(args.dir)
@@ -153,7 +166,6 @@ def main():
             flip_horizontally(args.dir, args.out_dir)
         case "crop":
             crop_specific_region(args.dir,args.out_dir, args.width, args.height, args.x, args.y)
-            
 
 if __name__ == "__main__":
     main()
